29c96078db0e929d0d8bc81aa764d4d7308ae3cc
nicobared <nicobared@gmail.com>
2019-12-10 17:11:04 -0500

    pushing backend

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
-const router = require('express').Router();
+const express = require('express');
+const router  = express.Router();
+const User    = require('../models/User');
+const Trip  = require('../models/Trip');
 
 router.get('/', (req, res, next) => {
   res.status(200).json({ msg: 'Working' });
 });
+
+
+
+router.get('/mytrips/:id', (req, res, next)=>{
+
+  // console.log(req.params.id)
+
+  console.log("This is the my trips route")
+
+  Trip.findById(req.params.id)
+  .then(data => {
+    console.log(data)
+    res.json(data)
+  })
+}
+
+)
+  
 
 module.exports = router;
