0c3d78b5fc65aa88fd8b1cbedeb5bf32dc6c5693
nicobared <nicobared@gmail.com>
2019-12-16 11:07:58 -0500

    trying to deploy

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,7 @@ const Trip  = require('../models/Trip');
 const Airport  = require('../models/Airport');
 
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({ msg: 'Working' });
-});
+
 
 
 
