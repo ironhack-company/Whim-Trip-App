05d927fe3ff216302e674d0227862a2907971fab
nicobared <nicobared@gmail.com>
2019-12-16 11:00:53 -0500

    trying to deploy

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use('/', index);
 app.use('/', auth);
 
 // Uncomment this line for production
-// app.get('/*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 module.exports = app;
