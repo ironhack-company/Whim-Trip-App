202e333c01b105ee2fbcce093c2861e10d9a403a
mendosaurus <masaki.endo@nex.nikkei.com>
2019-12-17 17:44:39 -0500

    tweak airportFrom function

--- frontend/src/components/Flights/FlightSearch.js
+++ frontend/src/components/Flights/FlightSearch.js
@@ -6,7 +6,7 @@ import "./FlightSearch2.css";
 
 import { Link, Redirect } from "react-router-dom";
 import Loader from "react-loader-spinner";
-import airports from '../../data/airports';
+import airports from "../../data/airports";
 
 export class FlightSearch extends Component {
   state = {
@@ -32,7 +32,7 @@ export class FlightSearch extends Component {
     ]
   };
   componentDidMount() {
-    console.log(airports)
+    console.log(airports);
     console.log("Components!");
     // console.log(this.props)
     axios.get("http://whim-travel.co/flight-search").then(data =>
@@ -80,20 +80,21 @@ export class FlightSearch extends Component {
   };
 
   getFlights = () => {
-
-    let cityFrom = this.state.searchQuery
-
-
+    let cityFrom = this.state.searchQuery;
 
     let airportFrom = airports.find(airport => {
-      return (airport.city.toLowerCase() == cityFrom || airport.iata_code == cityFrom) // airport.state == cityFrom ||
-    })
+      return (
+        airport.city == cityFrom ||
+        airport.city.toLowerCase() == cityFrom ||
+        airport.city.toUpperCase() == cityFrom ||
+        airport.iata_code == cityFrom
+      );
+    });
 
+    let from = airportFrom ? airportFrom.iata_code : cityFrom;
 
-    let from = airportFrom ? airportFrom.iata_code : cityFrom
-
-
-    const RAPIDAPI_API_URL = `https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=${from || this.state.searchCode}`; // if you fetch in componentDidMount it returns error because there is no origin when the page is loaded
+    const RAPIDAPI_API_URL = `https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=${from ||
+      this.state.searchCode}`; // if you fetch in componentDidMount it returns error because there is no origin when the page is loaded
     console.log(this);
     axios
       .get(RAPIDAPI_API_URL, { headers: this.props.headers }) //use token to get data
